ae6f2a6f11fbff60de7fcd7f436455d7731f02d0
Trinity Gordon <149403530+trinitygordon@users.noreply.github.com>
2024-04-06 17:29:45 -0400

    Update user_settings.dart
    fixed error

--- flutter/voting_app/lib/user/user_settings.dart
+++ flutter/voting_app/lib/user/user_settings.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:voting_app/user/changePswd.dart';
+import 'package:voting_app/changePswd.dart';
 import 'package:voting_app/main.dart';
 
 class UserSettings extends StatefulWidget {
