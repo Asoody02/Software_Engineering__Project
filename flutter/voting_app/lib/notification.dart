fb8b84e38dadd31762faaa6d17e46a93691d826b
Joshua Yu <joshua.yu831@gmail.com>
2024-04-09 20:44:28 -0400

    notification stuff

--- flutter/voting_app/lib/notification.dart
+++ flutter/voting_app/lib/notification.dart
@@ -1,15 +1,22 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
-
-final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void sendNotification(String token, String title, String body) async {
-  await _firebaseMessaging.send(
-    RemoteMessage(
-      data: {
-        'title': title,
-        'body': body,
-      },
-      to: token,
-    ),
+  final response = await http.post(
+    Uri.parse('http://127.0.0.1:5000/send_notification'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'token': token,
+      'title': title,
+      'body': body,
+    }),
   );
+
+  if (response.statusCode == 200) {
+    // print('Notification sent successfully');
+  } else {
+    // print('Failed to send notification: ${response.body}');
+  }
 }
