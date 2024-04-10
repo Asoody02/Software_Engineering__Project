b3922ebfc9e27d86948ecf4372d0a6a87e1a0a9d
tamelon <38846671+tamelon@users.noreply.github.com>
2024-04-09 22:20:11 -0400

    Create logout.py

---
+++ Templates/Backend/logout.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+from app import app
+from Database import Database, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
+from flask import session, jsonify
+from flask import Flask, request, jsonify
+
+@app.route('/logout')
+def logout():
+    # Check if 'username' is actually in session before popping it to avoid KeyError
+        if 'username' in session:
+            session.pop('username', None)
+    # Redirect to the login page, not the function name 'login'
+        return redirect(url_for('index'))
